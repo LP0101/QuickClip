ff289ce592bd80e6b3731809702657472758ee83
Luca Popesco <luca@popesco.io>
2022-04-27 13:53:56 -0400

    Use custom static

--- app.py
+++ app.py
@@ -2,13 +2,13 @@ import os
 import string
 import random
 
-from flask import Flask, flash, render_template, request, redirect, url_for
+from flask import Flask, flash, render_template, request, redirect, send_from_directory
 # please note the import from `flask_uploads` - not `flask_reuploaded`!!
 # this is done on purpose to stay compatible with `Flask-Uploads`
 
 app = Flask(__name__)
 app.secret_key = os.urandom(24)
-app.config["UPLOAD_FOLDER"] = "static"
+app.config["UPLOAD_FOLDER"] = os.getenv("QUICKCLIP_LIBRARY_PATH")
 
 def random_filename(length):
     str = string.ascii_lowercase
@@ -33,6 +33,15 @@ def upload_video():
         flash('Video uploaded and displayed below')
         return redirect(f"/v/{filename}")
 
-@app.route('/v/<filename>')
+@app.route('/files/<path:filename>')
+def custom_static(filename):
+    full_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+
+    if os.path.isfile(full_path):
+        return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+    else:
+        return "File not found", 400
+
+@app.route('/v/<path:filename>')
 def display_video(filename):
-    return render_template('clip.html', filename=f"/static/{filename}")
+    return render_template('clip.html', filename=f"/files/{filename}")
