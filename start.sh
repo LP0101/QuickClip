496dbbb28e9f68634d1f26bf0e8e254f38da761c
Luca Popesco <luca@popesco.io>
2022-04-28 22:28:55 -0400

    Added x-forwarded-for in logs

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 #! /bin/sh
-uwsgi --http 0.0.0.0:5000 --module quickclip:app --master --processes 3 --threads 3
+uwsgi --http 0.0.0.0:5000 --module quickclip:app --master --processes 3 --threads 3 --log-x-forwarded-for
