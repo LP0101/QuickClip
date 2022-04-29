44a4f6ca0ab3ffee592dffa179781aeacde7ff70
Luca Popesco <luca@popesco.io>
2022-04-28 23:02:05 -0400

    Why was I using 3 processes?

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 #! /bin/sh
-uwsgi --http 0.0.0.0:5000 --module quickclip:app --master --processes 3 --threads 3 --log-x-forwarded-for
+uwsgi --http 0.0.0.0:5000 --module quickclip:app --master --processes 4 --threads 4 --log-x-forwarded-for
